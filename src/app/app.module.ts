b4614d5696afecba07ae8a0cd5ad42982f42faf9
Tasnem Osama <temoabosamra@gmail.com>
2025-02-20 14:56:16 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';  
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './user/user.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([]),
+    FormsModule,
+    UserComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  standalone: false,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: './user.component.html', // تأكد أن الملف بنفس الاسم
+  styleUrls: ['./user.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule] 
 })
 export class UserComponent {
+  users = [
+    { name: 'tasneem', email: 'tasneemosama288@gmail.com', id: '1' }
+  ];
 
+  newUser = { name: '', email: '', id: '' };
+
+  addUser() {
+    if (this.newUser.name && this.newUser.email && this.newUser.id) {
+      this.users.push({ ...this.newUser });
+      this.newUser = { name: '', email: '', id: '' };
+    }
+  }
+
+  editUser(user: any) {
+    this.newUser = { ...user };
+    this.deleteUser(user.id);
+  }
+
+  deleteUser(id: string) {
+    this.users = this.users.filter(user => user.id !== id);
+  }
 }
